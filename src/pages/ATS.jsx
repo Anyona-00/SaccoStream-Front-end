b9b01656ae6a0a0ec654ecc88cf062c40ee01ccd
Anyona-00 <ombasaanyona@gmail.com>
2025-04-03 20:33:06 +0300

    testing new feature

--- src/pages/ATS.jsx
+++ src/pages/ATS.jsx
@@ -35,7 +35,7 @@ const CheckATSForm = () => {
     try {
       // Replace with your n8n webhook URL
       const webhookUrl =
-        "https://anyona.app.n8n.cloud/webhook-test/5a2624e7-33c7-4b96-b025-2526f0423c05";
+        "https://anyona.app.n8n.cloud/webhook/5a2624e7-33c7-4b96-b025-2526f0423c05";
       const response = await fetch(webhookUrl, {
         method: "POST",
         body: formData,
